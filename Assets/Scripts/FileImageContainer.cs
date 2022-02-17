27715ef74f1ee40dacb6b1d1324e43f3bc871ee3
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-02-17 14:01:05 +0100

    Adding FileImageLoader and FileImageContainer for UI instantiate based on Gallery folder (runtime directory) - still working on getting dates from files

---
+++ Assets/Scripts/FileImageContainer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileImageContainer : MonoBehaviour
+{
+    [SerializeField]
+    private Image fileImage = null;
+
+    [SerializeField]
+    private Text fileName = null;
+
+    [SerializeField]
+    private Text fileDate = null;
+
+    public void Initialize(string fileName, Sprite fileImage)
+    {
+        this.fileName.text = fileName;
+        this.fileImage.sprite = fileImage;
+    }
+}
---
+++ Assets/Scripts/FileImageLoader.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileImageLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string chosenFileType = "png";
+
+    [SerializeField]
+    private string chosenFolderName = "Gallery";
+
+    [SerializeField]
+    private FileImageContainer fileImagePrefab = null;
+
+    [SerializeField]
+    private Transform fileImageParent = null;
+
+    protected void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        string basePath = $"{Application.dataPath}/{chosenFolderName}";
+
+        foreach (Transform child in fileImageParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        
+        foreach (string specificFilePath in Directory.EnumerateFiles(basePath, $"*.{chosenFileType}"))
+        {
+            FileImageContainer fileImageContainer = Instantiate(fileImagePrefab, fileImageParent);
+            fileImageContainer.Initialize(Path.GetFileName(specificFilePath), 
+                LoadSpriteFromFile(specificFilePath));
+        }
+
+    }
+
+    public Texture2D LoadTextureFromFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            byte[] bytes = File.ReadAllBytes(filePath);
+            Texture2D tex = new Texture2D(0, 0); // parameters here don't matter, because of loading image in the next step
+            tex.LoadImage(bytes);
+            return tex;
+        }
+
+        return null;
+    }
+
+    public Sprite LoadSpriteFromFile(string filePath)
+    {
+        Texture2D tex = LoadTextureFromFile(filePath);
+
+        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100f);
+        return sprite;
+    }
+}
